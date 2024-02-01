b631ef688e45e51e692aeb929069349168d2bebf
meastman123 <113388262+meastman123@users.noreply.github.com>
2024-02-01 11:54:54 +0000

    New Changes 1.2.24 Lesson

--- email.php
+++ email.php
@@ -1,7 +1,24 @@
 <?php
-if ($name = $_POST["name"];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		$name = $_POST["name"];
     $email = $_POST["email"];
     $phone = $_POST["phone"];
     $description = $_POST["description"];
 
-		%)
+    $to = "17EastM@gordons.school";
+		$subject = "New booking Inquiry";
+		$message = "Name: $name\n";
+		$message = "Email: $email\n";
+		$message = "Phone: $phone\n";
+		$message = "Description: $description\n";
+
+		$headers = "From $email\r\n";
+		$headers = "Reply-To $email\r\n";
+
+		if (mail($to, $subject, $message, $headers)) {
+			echo "success";
+		} else {
+			echo "error";
+		}
+}
+?>
