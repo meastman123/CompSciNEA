21a09b0dd35e7b05498fe86dae541fc64ebeb698
meastman123 <113388262+meastman123@users.noreply.github.com>
2024-02-09 09:19:35 +0000

    Commit 9.2.24 PE Lesson

--- replit.nix
+++ replit.nix
@@ -1,5 +1,8 @@
 { pkgs }: {
   deps = [
+    pkgs.openssh_gssapi
+    pkgs.openssh_hpn
+    pkgs.openssh
     pkgs.nodePackages.vscode-langservers-extracted
     pkgs.nodePackages.typescript-language-server  
   ];
